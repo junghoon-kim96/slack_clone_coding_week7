0dd43560b1e04df2ee539a1e6942fedc37ed6e0a
ys <oneyul0313@naver.com>
2022-06-20 10:54:57 +0900

    스타트페이지 변경

--- src/pages/Start/Start.js
+++ src/pages/Start/Start.js
@@ -5,10 +5,11 @@ function Start() {
     <div className="App">
       <header className="App-header">
         <img className="App-logo" src='https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png' className="App-logo" alt="logo" />
-        <p>
-        슬랙 시작하기
-        </p>
-        <Link to= '/login'> 로그인 하러가기</Link>
+        
+        
+        <Link to= '/login'  
+        style={{backgroundColor: 'lightblue', marginTop: '30px', padding:'5px', borderRadius: '5px'
+        ,textDecoration: 'none', color: 'white'}}> 슬랙 시작하기</Link>
       </header>
     </div>
   );
